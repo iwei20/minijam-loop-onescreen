7b6a7962951294c3b5cf8bdcb8e2cf9c762164c2
iwei20 <ivanw8288@gmail.com>
2022-09-17 19:26:09 -0400

    Update turn()

--- Assets/GameState/Tiles/Portal.cs
+++ Assets/GameState/Tiles/Portal.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Portal : Tile
 {
+    public Portal(int x, int y) 
+    {
+        super(x, y);
+        super.tag = "portal";
+    }
+    
     public override void onCollision(BoardManager bm) {
         BoardManager.turn();
     }
